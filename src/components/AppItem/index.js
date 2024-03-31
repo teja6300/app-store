c6dd2e2c99e2b8266701538927c1b1096761c7f8
teja6300 <218w1aa1239@vrsec.ac.in>
2024-03-31 06:21:16 +0000

    app store

---
+++ src/components/AppItem/index.js
@@ -0,0 +1,16 @@
+// Write your code here
+import './index.css'
+
+const AppItem = props => {
+  const {appDetails} = props
+  const {appName, imageUrl} = appDetails
+
+  return (
+    <li className="app-item-container">
+      <img src={imageUrl} className="app-logo" alt={appName} />
+      <p className="app-name">{appName}</p>
+    </li>
+  )
+}
+
+export default AppItem
---
+++ src/components/TabItem/index.js
@@ -0,0 +1,21 @@
+// Write your code here
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, updateTabId, isActive} = props
+  const {tabId, displayText} = tabDetails
+  const onClickButton = () => {
+    updateTabId(tabId)
+  }
+
+  const tabBtnClassName = isActive ? 'tab-button active' : 'tab-button'
+  return (
+    <li className="tab-item-container">
+      <button className={tabBtnClassName} type="button" onClick={onClickButton}>
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
